6525f21591cef2216be7224a43b93e766d63e384
foxmulder4711 <carsten.schild@fujitsu.com>
2025-07-16 12:24:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -22,7 +22,8 @@ st.write(
 
 #st.write("You selected:", selected_value)
 
-session = get_active_session()
+cnx = st.connection("snwoflake")
+session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
